3691050d65ac825598146c4cc15a07b8addf507e
mcquadmj <mmcquad.17@gmail.com>
2022-02-13 16:45:40 -0500

    Initial Setup for Vehicle and Photo data classes (using lombak)

---
+++ src/main/java/com/hotjava/enterprise/dto/Vehicle.java
@@ -0,0 +1,16 @@
+package com.hotjava.enterprise.dto;
+
+import lombok.Data;
+
+public @Data
+class Vehicle {
+    private int vehicleSubmissionID;
+    private String vehicleOwnerName;
+    private String vehicleDescription;
+    private String vehicleYear;
+    private String vehicleMake;
+    private String vehicleModel;
+    private String vehicleScore;
+
+    private Photo photo;
+}

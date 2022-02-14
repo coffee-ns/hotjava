12aa7943da43f165579906678abd111b119e0c60
mcquadmj <mmcquad.17@gmail.com>
2022-02-14 10:56:44 -0500

    Initial Setup for apps controller

--- src/main/java/com/hotjava/app/hotjava/HotJavaController.java
+++ src/main/java/com/hotjava/app/hotjava/HotJavaController.java
@@ -5,16 +5,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+
+
+/**
+ * The controller for HotJava REST endpoints and web UI
+ * <p>
+ * This class handles the CRUD operations for HotJava Vehicles, via HTTP actions.
+ * </p>
+ * <p>
+ * This class also serves HTML based web pages, for UI interactions with vehicles.
+ * </p>
+ */
 @Controller
 public class HotJavaController {
 
     /**
-     * Handles / endpoint
-     * @return index view
+     * Handle the / endpoint
+     * @return
      */
     @RequestMapping("/")
-    public String index()
-    {
-        return "index";
+    public String index() {
+        return "start";
     }
+
+
 }

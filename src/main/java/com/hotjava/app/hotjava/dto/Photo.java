0fb237872cc2ee0f55242c5c8ba32f3844d4e28f
mcquadmj <mmcquad.17@gmail.com>
2022-02-14 10:58:12 -0500

    Initial Setup for Vehicle and Photo data classes (using lombok)

---
+++ src/main/java/com/hotjava/app/hotjava/dto/Photo.java
@@ -0,0 +1,15 @@
+package com.hotjava.app.hotjava.dto;
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+
+public @Data
+class Photo {
+    private int photoId;
+    private String path;
+    private String fileName;
+    private String comments;
+}

bac47b8d2943227c0cfceae45e7184e840811db6
mcquadmj <mmcquad.17@gmail.com>
2022-03-20 17:37:37 -0400

    small change to fix build/debug

--- src/main/java/com/hotjava/app/hotjava/dao/VehicleSQLDAO.java
+++ src/main/java/com/hotjava/app/hotjava/dao/VehicleSQLDAO.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 //@Repository("vehicleDAO")
 @Repository
-@Profile("dev")
+@Profile({"dev", "default"})
 public class VehicleSQLDAO implements IVehicleDAO{
 
     @Autowired

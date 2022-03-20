80c7a85e71a5d620a1b82ffa2da18148d41ffb58
mcquadmj <mmcquad.17@gmail.com>
2022-03-20 17:30:33 -0400

    Setting up environments for testing and Circle CI

--- src/main/java/com/hotjava/app/hotjava/dao/VehicleRepository.java
+++ src/main/java/com/hotjava/app/hotjava/dao/VehicleRepository.java
@@ -1,7 +1,9 @@
 package com.hotjava.app.hotjava.dao;
 
 import com.hotjava.app.hotjava.dto.Vehicle;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.repository.CrudRepository;
 
+@Profile("!test")
 public interface VehicleRepository extends CrudRepository<Vehicle, Integer> {
 }
--- src/main/java/com/hotjava/app/hotjava/dao/VehicleSQLDAO.java
+++ src/main/java/com/hotjava/app/hotjava/dao/VehicleSQLDAO.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 //@Repository("vehicleDAO")
 @Repository
-@Profile({"dev", "default"})
+@Profile("dev")
 public class VehicleSQLDAO implements IVehicleDAO{
 
     @Autowired

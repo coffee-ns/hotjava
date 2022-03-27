9bbfb63c7df8da06df980734a2263a5c99065628
Jacob Gormly <gormlyja@mail.uc.edu>
2022-03-27 19:07:14 -0400

    Javax persistence aws not being used, probably because used code was commented out. Keep development utilizing this import in a seperate branch to keep from having to comment out a lot of code.

--- src/main/java/com/hotjava/app/hotjava/dto/Photo.java
+++ src/main/java/com/hotjava/app/hotjava/dto/Photo.java
@@ -2,13 +2,10 @@ package com.hotjava.app.hotjava.dto;
 
 import lombok.Data;
 import lombok.ToString;
-
-import javax.persistence.*;
 import java.io.Serializable;
 
 
 public @Data
-//@Table(name="photos")
 class Photo implements Serializable {
 
 
@@ -16,9 +13,5 @@ class Photo implements Serializable {
     private String path;
     private String fileName;
     private String comments;
-
-   // @ToString.Exclude
-   // @OneToOne
-   // @JoinColumn(name="vehicle_id",referencedColumnName = "id" )
     private int vehicleID;
 }
